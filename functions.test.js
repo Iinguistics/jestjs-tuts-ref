dc0aa61bf1ffc5fbeaea0c8b1d3848c8b7f6a259
James <Thylinguistics@gmail.com>
2021-03-13 17:13:52 -0800

    ...

--- functions.test.js
+++ functions.test.js
@@ -1,6 +1,32 @@
 const functions = require('./functions');
 
 
+// before & after ea test it will run these functions
+// beforeEach(()=> initDatabase());
+// afterEach(()=> closeDatabase());
+
+// before all tests runs initdb function then after all test are ran it runs closeDB function
+// beforeAll(()=> initDatabase());
+// afterAll(()=> closeDatabase());
+
+// const initDatabase = () => console.log('Database Initialized...');
+// const closeDatabase = () => console.log('Database Closed...');
+
+const myBeverage = {
+    delicious: true,
+    sour: false,
+  };
+  
+  describe('my beverage', () => {
+    test('is delicious', () => {
+      expect(myBeverage.delicious).toBeTruthy();
+    });
+  
+    test('is not sour', () => {
+      expect(myBeverage.sour).toBeFalsy();
+    });
+  });
+
 
 test('adds 1 + 2 to equal 3', ()=>{
     expect(functions.add(1,2)).toBe(3);
